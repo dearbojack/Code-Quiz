2455e47b40ece7b693db33219329e739d9d3cbff
Dali Han <ross4han@gmail.com>
2023-01-17 20:47:12 +0000

    populate quizQuestions

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+const quizQuestions = [
+    {
+      question: "What is the most popular front-end web development language?",
+      choices: ["JavaScript", "PHP", "Python", "C++"],
+      correctAnswer: "JavaScript"
+    },
+    {
+      question: "What is the most popular front-end web development library?",
+      choices: ["jQuery", "React", "Angular", "Vue"],
+      correctAnswer: "jQuery"
+    },
+    {
+      question: "What is the most popular front-end web development framework?",
+      choices: ["Bootstrap", "React", "Angular", "Vue"],
+      correctAnswer: "React"
+    },
+    {
+      question: "How do you make a list that lists the items with numbers?",
+      choices: ["<ul>", "<ol>", "<list>", "<li>"],
+      correctAnswer: "<ol>"
+    },
+    {
+      question: "How do you select an element with id 'demo'?",
+      choices: ["demo", "#demo", ".demo", "demo"],
+      correctAnswer: "#demo"
+    },
+    {
+      question: "What is the correct HTML tag for creating a hyperlink?",
+      choices: ["<a></a>", "<p></p>", "<em></em>", "<s></s>"],
+      correctAnswer: "<a></a>"
+    },
+    {
+      question: "What is the most popular front-end web development language for mobile development?",
+      choices: ["React Native", "Ionic", "Flutter", "NativeScript"],
+      correctAnswer: "React Native"
+    },
+    {
+      question: "What is the most popular front-end web development language for creating user interfaces?",
+      choices: ["HTML", "CSS", "JavaScript", "TypeScript"],
+      correctAnswer: "HTML"
+    },
+    {
+      question: "What is the most popular front-end web development language for creating dynamic webpages?",
+      choices: ["PHP", "JavaScript", "Python", "Ruby"],
+      correctAnswer: "JavaScript"
+    },
+    {
+      question: "What is the most popular front-end web development language for creating web applications?",
+      choices: ["JavaScript", "PHP", "Python", "Ruby"],
+      correctAnswer: "JavaScript"
+    }
+  ];
