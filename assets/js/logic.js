f2f1286133f8a3ca84fa6e0306397b824d828a87
Dali Han <ross4han@gmail.com>
2023-01-18 09:12:21 +0000

    countdown timer TODO

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,10 +8,15 @@ var time = 90;
 document.getElementById('start').addEventListener("click", function(){
     let timer = setInterval(function (){
         time--;
+
+        // * TODO penalty after wrong answer
+
         document.getElementById('time').innerHTML = time;
         
-        if (count === 0) {
+        if (time === 0) {
             clearInterval(timer);
+
+            // * TODO display end screen
         }
     }, 1000);
 });
