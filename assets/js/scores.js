83302bf5973b2e48a002e4ef34a1619107e6c489
Dali Han <ross4han@gmail.com>
2023-01-18 22:59:29 +0000

    tidy the code

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -12,6 +12,7 @@ function printScores() {
     // print and show
     for (var i = 0; i < keys.length; i++) {
         // the createElment should be in the for loop or it only creates 1 records TT
+        // took me so long to debug this TT
         var scoreRecords = document.createElement('li');
         
         scoreRecords.innerHTML = keys[i] + ' - ' + localStorage[keys[i]];
