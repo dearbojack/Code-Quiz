ed3023d796deafe7b0a15a449aead98b0a892a33
Dali Han <ross4han@gmail.com>
2023-01-18 09:09:56 +0000

    basic countdown timer

---
+++ assets/js/logic.js
@@ -0,0 +1,48 @@
+
+
+// countdown timer function
+
+// after user click start buttion start the countdown
+var time = 90;
+
+document.getElementById('start').addEventListener("click", function(){
+    let timer = setInterval(function (){
+        time--;
+        document.getElementById('time').innerHTML = time;
+        
+        if (count === 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+});
+
+
+
+// randomize the arr and loop through it
+
+function randomizer(arr) {
+    var arr = [];
+
+    // randomize the order of the arr
+    arr.sort(() => Math.random() - 0.5);
+
+    // loop through it
+    // display questions
+}
+
+// display the questions in quiz format
+
+function questionDisplay () {
+    for (let i = 0; i < quizQuestions.length; i++) {
+        const question = quizQuestions[i].question;
+        const choices = quizQuestions[i].choices;
+        const correctAnswer = quizQuestions[i].correctAnswer;
+
+        console.log(question);
+        console.log(choices);
+        console.log(correctAnswer);
+        
+    }
+}
+
+questionDisplay();
