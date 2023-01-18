f586518176d22c4849ca1d1f56617b6542bc0c88
Dali Han <ross4han@gmail.com>
2023-01-18 19:31:34 +0000

    add eventlistener to li items

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -23,62 +23,60 @@ function countDown() {
 
 var index = 0;
 
+// create an ul to hold the choices
+const list = document.createElement('ul');
+list.setAttribute('id', 'list');
+
 // random the question order
 quizQuestions.sort(() => Math.random() - 0.5);
 
 // func to display questions
 
 function qDisplay(index) {
-    document.getElementById("start-screen").className ="hide";
-    document.getElementById("questions").className ="";
     document.getElementById("question-title").innerHTML = quizQuestions[index].question;
 
-    let list = document.createElement('ul');
-    
     for (let i = 0; i < quizQuestions[index].choices.length; i++) {
-        let listItem = document.createElement('button');
+        let listItem = document.createElement('li');
         listItem.innerHTML = quizQuestions[index].choices[i];
-        list.appendChild(listItem);
-        document.getElementById("choices").appendChild(list);
+        document.getElementById("choices").appendChild(listItem); 
+
+        listItem.addEventListener('click', function(e) {
+            if (e.target.innerText === quizQuestions[index].correctAnswer) {
+                console.log("ohye");
+                console.log(e.target.innerText);
+                console.log(index);
+                nextQestion(index);
+            } else {
+                console.log("ohno");
+                console.log(e.target.innerText);
+                console.log(index);
+                nextQestion(index);
+            };
+        });
+        
     };
+
+    // 
+    document.getElementById("choices").appendChild(list);
+};
+
+function nextQestion(index) {
+    index++ ;
+    document.getElementById("choices").innerHTML = '';
+    qDisplay(index);
 }
 
+// listen to click on the start button
 document.getElementById('start').addEventListener("click", function(){
-
+    // start the timer
     countDown(time);
     qDisplay(index);
-    index++;
-    // document.getElementById("start-screen").className ="hide";
-    // document.getElementById("questions").className ="";
-    // document.getElementById("question-title").innerHTML = quizQuestions[index].question;
 
-    
-    // let list = document.createElement('ul');
-    
-    // for (let i = 0; i < quizQuestions[index].choices.length; i++) {
-
-    //     let listItem = document.createElement('button');
-    //     listItem.innerHTML = quizQuestions[index].choices[i];
-    //     list.appendChild(listItem);
-    //     document.getElementById("choices").appendChild(list);
-
-    //     // check if the answer is correct
-    //     listItem.addEventListener("click", function() {
-    //         if(quizQuestions[index].choices[i] === quizQuestions[index].correctAnswer) {
-    //             userScore++;
-    //             console.log("Your score is " + userScore);
-    //         } else {
-
-    //             // if wrong, time - 10s
-    //             time = time - 10;
-    //             console.log(time);
-    //         };   
-        // });
-
-    // };
-
-
-
-
-    // index ++;
+    // unhide the question & hide the start-screen
+    document.getElementById("start-screen").className ="hide";
+    document.getElementById("questions").className ="";
 });
+
+
+
+
